d67f873948b676048b70664b19217fd51c74b733
greenhornet63603 <greenhornet63603@gmail.com>
2025-11-28 20:31:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("8231825546:AAE2a3lz3YJsnpZPqD1xEbkI9JusKZ5wmmM")
-    API_ID = int(os.environ.get("27400172"))
+    BOT_TOKEN = os.environ.get("8231825546:AAE2a3lz3YJsnpZPqD1xEbkI9JusKZ5wmmM")       # Environment variable name
+    API_ID = int(os.environ.get("27400172"))        # Must be set as env var
     API_HASH = os.environ.get("56d0a75c5f9a9de6beb5452aa63c2d36")
-    AUTH_USERS = os.environ.get("7540570087")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "7540570087") # Comma-separated IDs
