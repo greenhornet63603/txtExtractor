15e8e1eff569b83135e0f85709ca71955a993fd5
greenhornet63603 <greenhornet63603@gmail.com>
2025-11-28 19:55:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     BOT_TOKEN = os.environ.get("8231825546:AAE2a3lz3YJsnpZPqD1xEbkI9JusKZ5wmmM")
     API_ID = int(os.environ.get("27400172"))
     API_HASH = os.environ.get("56d0a75c5f9a9de6beb5452aa63c2d36")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "7540570087")
+    AUTH_USERS = os.environ.get("7540570087")
